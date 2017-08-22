c6da91d075ad727afc2c4726b28d2654026a8aa1
dgriego <eggomygriego@gmail.com>
2017-08-22 16:34:53 -0700

    redo of odd numbers exercise

--- odd_numbers.js
+++ odd_numbers.js
@@ -1,21 +1,24 @@
-function loggOddNumbers(max) {
-  for (var i = 1; i < max + 1; i++) {
-    if (i % 2 !== 0) {
+// Write a function that takes a positive integer as an argument,
+// and logs all the odd numbers from 1 to the passed in number (inclusive).
+// All numbers should be logged on separate lines.
+function logOddNumbers(int) {
+  for (var i = 1; i <= int; i++) {
+    if (i % 2 === 1) {
       console.log(i);
     }
   }
 }
 
-// assignment solution
-function logOddNumbers(number) {
-  for (var i = 1; i <= number; i += 2) {
-    console.log(i);
+// incrementing the numbers by 2 instead of using conditional
+function logOddNumbers(int) {
+  for (var i = 1; i <= int; i = i + 2) {
+    console.log(i)
   }
 }
 
-// alternative
-function logOddNumbers(number) {
-  for (var i = 1; i<= number; i += 1) {
+// checking for even, then using continue
+function logOddNumbers(int) {
+  for (var i = 1; i <= int; i = i + 2) {
     if (i % 2 === 0) {
       continue;
     }
