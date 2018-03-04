638561f08aa5852784a3f3f8e0305215392f3e0e
dgriego <eggomygriego@gmail.com>
2018-03-03 16:52:24 -0800

    redo of odd_numbers.js

--- odd_numbers.js
+++ odd_numbers.js
@@ -1,28 +1,26 @@
 // Write a function that takes a positive integer as an argument,
 // and logs all the odd numbers from 1 to the passed in number (inclusive).
-// All numbers should be logged on separate lines.
-function logOddNumbers(int) {
-  for (var i = 1; i <= int; i++) {
+// All numbers should be logged on separate lines
+
+// 1, 0, 2, 3
+// but not -1, -2, -3
+// what if non integer?
+// -- guard clause to check if it is a Number, if not then just return and display an error message
+// what if the argument is less than 1?
+// -- guard clause to check if it is > 1
+
+
+// input 1, 2, 3, 4, 5, 6
+// output a log of the numbr from 1 to the passed in argument
+
+// start a loop that breaks at the number passed in
+
+function oddNumbers(num) {
+  for (var i = 1; i <= num; i++) {
     if (i % 2 === 1) {
-      console.log(i);
+      console.log(i)
     }
   }
 }
 
-// incrementing the numbers by 2 instead of using conditional
-function logOddNumbers(int) {
-  for (var i = 1; i <= int; i = i + 2) {
-    console.log(i)
-  }
-}
-
-// checking for even, then using continue
-function logOddNumbers(int) {
-  for (var i = 1; i <= int; i = i + 2) {
-    if (i % 2 === 0) {
-      continue;
-    }
-
-    console.log(i);
-  }
-}
+oddNumbers(21);
